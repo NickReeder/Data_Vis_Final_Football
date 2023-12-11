f73716e3e5463aa83f502c3de84350124a34fd74
NickReeder <123888815+NickReeder@users.noreply.github.com>
2023-12-11 16:47:51 -0500

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -20,7 +20,7 @@ st.sidebar.success("Select a plot above.")
 st.markdown(
     """
     The goal of VISTA is to provide the user with a quick and digestable snapshot of a teams perfomance through a visiual medium without comprimising on detail. 
-    Data was collected from PFF Ultimate and contains play-by-play data from every NFL game for the last 10 seasons! A series of statistics were generated,
+    Data was collected from PFF Ultimate and contains data on nearly every play from every NFL game for the last 10 seasons! A series of statistics were generatad,
     and are described below.
 
     * Offensive Efficency: The average yards gained per play
@@ -33,13 +33,15 @@ st.markdown(
 
     Please click through the pages to view a more in depth look at each team.
 
-    The first and second page provide an overview of the league, comparing each teams overall offensive and defense efficency.
+    The first two pages provide an overview of the leauge, comparing each teams overall offensive and defense efficency.
+    
+    The thrid page is a ridgeline plot that____
+    
+    The forth page allows the user to compare two teams over a variety of statsitcs in a radar plot
 
-    The third page _____
+    The fith shows the user how a teams play callig has changed over time   
 
-    The fourth page allows the user to compare two teams over a variety of statsitcs in a radar plot.
 
-    The fifth page shows the user how a selected team's play calling has changed over time.
 
 
 
