474a0452b9b2c6ed8d9f0b0f96052ad98f2221fc
mmambler <mmambler@email.wm.edu>
2023-12-11 16:38:29 -0500

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -33,13 +33,13 @@ st.markdown(
 
     Please click through the pages to view a more in depth look at each team.
 
-    The first two pages provide an overview of the leauge, comparing each teams overall offensive and defense efficency.
+    The first and second page provide an overview of the league, comparing each teams overall offensive and defense efficency.
 
-    The third page allows the user to compare two teams over a variety of statsitcs in a radar plot
+    The third page _____
 
-    The fourth shows the user how a teams play callig has changed over time   
+    The fourth page allows the user to compare two teams over a variety of statsitcs in a radar plot.
 
-    The fith page _____
+    The fifth page shows the user how a selected team's play calling has changed over time.
 
 
 
