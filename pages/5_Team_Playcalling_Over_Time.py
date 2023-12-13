00374211b4941dd76f4016474611b0f6ed430bb5
mmambler <mmambler@email.wm.edu>
2023-12-12 21:46:37 -0500

    Update 5_Team_Playcalling_Over_Time.py

--- pages/5_Team_Playcalling_Over_Time.py
+++ pages/5_Team_Playcalling_Over_Time.py
@@ -74,7 +74,7 @@ def build_plots(team):
             line=dict(color=colors[1], width=line_size[0]),
             connectgaps=True,
             hovertemplate=
-            "<b>%{name}</b>" +
+            "<b>%{name:,}</b>" +
             "Plays: %{y:,}<br>" +
             "<extra></extra>"
         ))
