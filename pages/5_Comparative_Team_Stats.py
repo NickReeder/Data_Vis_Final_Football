5138c387e28ae4df70529fd285435393e84cd652
mmambler <mmambler@email.wm.edu>
2023-12-13 13:28:18 -0500

    Update 5_Comparative_Team_Stats.py

--- pages/5_Comparative_Team_Stats.py
+++ pages/5_Comparative_Team_Stats.py
@@ -106,12 +106,12 @@ def build_plots(t_1,t_2):
                             ,textposition= ['top right', 'top center', 'top left']
                             ,name = 'Passing Yards'
                             ), row = 2, col = 2)
-    fig.add_trace(go.Scatter(x = [t_1, '', t_2], y = [50, (50 + y_2[2]) / 2, y_2[2]] 
+    fig.add_trace(go.Scatter(x = [t_1, '', t_2], y = [50, (50 + y_2[4]) / 2, y_2[4]] 
                             ,mode ='markers+lines+text', text = [r_1, str(pct_change[2])+'%', r_2]
                             ,textposition= ['top right', 'top center', 'top left']
                             , name = 'Rushing Turnover Percentage'
                             ), row = 3, col = 1)
-    fig.add_trace(go.Scatter(x = [t_1, '', t_2], y = [50, (50 + y_2[3]) / 2, y_2[3]] 
+    fig.add_trace(go.Scatter(x = [t_1, '', t_2], y = [50, (50 + y_2[5]) / 2, y_2[5]] 
                             ,mode ='markers+lines+text', text = [r_1, str(pct_change[3])+'%', r_2]
                             ,textposition= ['top right', 'top center', 'top left']
                             , name = 'Passing Turnover Percentage'
